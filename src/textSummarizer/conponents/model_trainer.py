2626b226062133abcd66a72b5a616039701bbee5
LaveKumar <lavechauhan7856@gmail.com>
2024-11-24 18:08:56 +0530

    Model Evaluation Completed

--- src/textSummarizer/conponents/model_trainer.py
+++ src/textSummarizer/conponents/model_trainer.py
@@ -2,6 +2,7 @@ from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from datasets import load_dataset, load_from_disk
+from textSummarizer.entity import ModelTrainerConfig
 import torch
 
 
